8f6a2e72e21b45121989fd7304dd0c497a2bf252
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-08-21 14:29:33 +0530

    Approach 2 - Optimised using two pointer

--- Arrays/Medium/17_Container_With_Most_Water.py
+++ Arrays/Medium/17_Container_With_Most_Water.py
@@ -1,16 +1,38 @@
 from typing import List
 
+# class Solution:
+#     def maxArea(self, height: List[int]) -> int:
+#         max = 0
+#         for i in range(len(height)):
+#             for j in range(i, len(height)):
+#                 box_height = min(height[i], height[j])
+#                 width = j-i
+#                 if box_height * width > max:
+#                     max = box_height * width
+
+#         return max
+    
+    
+    
+# optimised solution
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        max = 0
-        for i in range(len(height)):
-            for j in range(i, len(height)):
-                box_height = min(height[i], height[j])
-                width = j-i
-                if box_height * width > max:
-                    max = box_height * width
-
-        return max
+        max_area = 0
+        left, right = 0, len(height) - 1
+        
+        while left < right:
+            min_height = min(height[left], height[right])
+            width = right - left
+            area = min_height * width
+            max_area = max(max_area, area)
+            
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        
+        return max_area
+    
     
 height = [int(x) for x in input().split()]
 s1 = Solution()
